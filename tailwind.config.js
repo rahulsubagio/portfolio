699dab34d20563ae7333f0c03d5fea62d4730483
rahulsubagio <rahulsubagio99@gmail.com>
2022-10-09 22:09:27 +0700

    tambah hex warna

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['index.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#7CC649',
+        dark: '#3F4754',
+      }
+    },
   },
   plugins: [],
 }
