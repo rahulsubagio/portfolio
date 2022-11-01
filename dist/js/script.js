4546dd4cb518872cb760b852ec6c585118520622
rahulsubagio <rahulsubagio99@gmail.com>
2022-11-01 14:37:12 +0700

    update footer & create modal

--- dist/js/script.js
+++ dist/js/script.js
@@ -77,7 +77,29 @@ function setup() {
 };
 /* ========== About Section End ========== */
 
+/* ========== Service Section Start ========== */
+const showModal = (openButton, modalContent) => {
+  const openBtn = document.getElementById(openButton);
+  const modalContainer = document.getElementById(modalContent);
 
+  if (openBtn && modalContainer) {
+    openBtn.addEventListener('click', () => {
+      modalContainer.classList.add('.show-modal');
+    });
+  }
+}
+
+showModal('#open-modal', '#modal-container');
+
+const closeBtn = document.querySelectorAll('.close-modal');
+
+function closeModal() {
+  const modalContainer = document.getElementById('#modal-container');
+  modalContainer.classList.remove('show-modal')
+}
+
+closeBtn.forEach(c => c.addEventListener('click', closeModal));
+/* ========== Service Section End ========== */
 
 
 /* ============================== Untuk testing aja ============================== 
